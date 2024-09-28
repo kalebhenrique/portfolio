b244343d11cb4ef391cbec3e88ac082e3bce4808
kalebhenrique <kalebhenriquebr@gmail.com>
2024-09-28 19:48:17 -0300

    Add @radix-ui/react-tooltip dependency and implement tooltip component

--- src/app/_components/introduction.tsx
+++ src/app/_components/introduction.tsx
@@ -1,28 +1,44 @@
 import ArrowDownAnimate from "./arrowDownAnimate";
 import CldImage from "./cldImage";
 import { MdInfoOutline } from "react-icons/md";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "~/components/ui/tooltip";
 
 export default function Introduction() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center py-36">
+      <div className="mx-6 flex flex-col items-center justify-center pb-20 pt-28">
         <div className="flex flex-row items-center">
           <CldImage
-            width="300"
-            height="300"
+            width="360"
+            height="360"
             src="celeste"
             alt="Montanha Celeste"
           />
-          <MdInfoOutline />
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <MdInfoOutline />
+              </TooltipTrigger>
+              <TooltipContent className="mr-2 w-32 bg-violeta-base bg-opacity-90 text-[11px] text-cinza-fundo backdrop-blur-sm">
+                <p>
+                  Montanha do jogo Celeste, o qual tem tema de superação e
+                  perseverança.
+                </p>
+                <p>Qualidades essenciais paras programadores!</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </div>
         <div className="py-3 text-center">
           <span>
             Sou apaixonado por criar experiências envolventes, acessíveis e
             centradas no usuário.
           </span>
-        </div>
-        <div>
-          <ArrowDownAnimate />
         </div>
       </div>
     </>
