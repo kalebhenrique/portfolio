256280205b1ab26358fa824b84fbeabf01a6aa4f
kalebhenrique <kalebhenriquebr@gmail.com>
2024-09-28 16:02:42 -0300

    add button and navbar

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,12 +1,20 @@
+"use client";
 import Link from "next/link";
+import { redirect } from "next/navigation";
+import { Button } from "~/components/ui/button";
 
 export default function Navbar() {
   return (
-    <div>
+    <div className="flex items-center justify-center gap-5 py-5">
       <Link href="/">Sobre mim</Link>
       <Link href="/">Habilidades</Link>
       <Link href="/">Projetos</Link>
-      <Link href="/">Contatos</Link>
+      <Button
+        className="rounded-full bg-violeta-base text-[13px] font-semibold text-cinza-fundo"
+        onClick={() => redirect("/")}
+      >
+        Contatos
+      </Button>
     </div>
   );
 }
