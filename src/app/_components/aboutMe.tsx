3fc0b528d4477c097d0d254e4604f97401a7c5e9
kalebhenrique <kalebhenriquebr@gmail.com>
2024-09-28 19:48:21 -0300

    add footer

---
+++ src/app/_components/aboutMe.tsx
@@ -0,0 +1,23 @@
+import CldImage from "./cldImage";
+
+export default function AboutMe() {
+  return (
+    <div className="flex flex-col items-center justify-center bg-roxo-fundo px-6">
+      <h1 className="pb-10 pt-16 text-center text-4xl text-violeta-titulo">
+        Eu sou um dev web atuando como front-end e bla bla.
+      </h1>
+      <CldImage
+        width="242"
+        height="242"
+        src="eu"
+        alt="Eu, Kaleb"
+        className="rounded-3xl"
+      />
+      <p className="my-7 text-justify">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer blandit
+        interdum magna, non consequat urna tincidunt dictum. Nam imperdiet porta
+        enim, nec semper quam commodo non. Vivamus vel enim ante.
+      </p>
+    </div>
+  );
+}
