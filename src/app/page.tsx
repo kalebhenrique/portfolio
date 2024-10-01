6246c36564d7c59666d7011ea49dfae788fd16e1
kalebhenrique <kalebhenriquebr@gmail.com>
2024-10-01 17:50:54 -0300

    Update layout and add snowflake animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import AboutMe from "./_components/aboutMe";
 import Footer from "./_components/footer";
 import Introduction from "./_components/introduction";
 import Navbar from "./_components/navbar";
+import Projects from "./_components/projects";
 
 export default function HomePage() {
   return (
     <main className="relative">
-      <Navbar />
       <Introduction />
       <AboutMe />
-      <Footer />
+      <Projects />
     </main>
   );
 }
