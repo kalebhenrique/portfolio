a41e1a96a9e4dbafc9a8be8600b3a4a58c286f51
kalebhenrique <kalebhenriquebr@gmail.com>
2024-10-06 19:09:00 -0300

    Add animation when component is going to be unmounted

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -3,7 +3,7 @@
 import "~/styles/globals.css";
 import { IoClose } from "react-icons/io5";
 import MessageWrapper from "./messageWrapper";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import MessageOption from "./messageOption";
 
 export default function Contact({ isOpen, onClose }) {
@@ -30,40 +30,41 @@ export default function Contact({ isOpen, onClose }) {
     },
   ];
 
-  if (!isOpen) return null;
-
   return (
-    <>
-      <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{
-          duration: 0.1,
-        }}
-        className="root-scrollbar fixed inset-0 z-20 flex h-[770px] w-full bg-black bg-opacity-50 backdrop-blur-3xl"
-      >
-        <div className="flex w-full flex-col bg-violeta-base bg-opacity-30">
-          <div className="flex items-center justify-between bg-violeta-base p-5">
-            <h2 className="text-cinza-fundo">Kaleb</h2>
-            <button
-              onClick={onClose}
-              className="text-cinza-fundo hover:text-gray-700"
-            >
-              <IoClose size={20} />
-            </button>
-          </div>
-          <div className="custom-scrollbar flex-grow overflow-y-auto">
-            <div className="flex flex-col space-y-2 p-4">
-              {introductionMessages.map((item, key) => (
-                <MessageWrapper key={key} {...item} />
-              ))}
-              {introductionOptions.map((item, key) => (
-                <MessageOption key={key} {...item} />
-              ))}
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 50 }}
+          transition={{
+            duration: 0.2,
+          }}
+          className="root-scrollbar fixed inset-0 z-20 flex h-[770px] w-full bg-black bg-opacity-50 backdrop-blur-3xl"
+        >
+          <div className="flex w-full flex-col bg-violeta-base bg-opacity-30">
+            <div className="flex items-center justify-between bg-violeta-base p-5">
+              <h2 className="text-cinza-fundo">Kaleb</h2>
+              <button
+                onClick={onClose}
+                className="text-cinza-fundo hover:text-gray-700"
+              >
+                <IoClose size={20} />
+              </button>
+            </div>
+            <div className="custom-scrollbar flex-grow overflow-y-auto">
+              <div className="flex flex-col space-y-2 p-4">
+                {introductionMessages.map((item, key) => (
+                  <MessageWrapper key={key} {...item} />
+                ))}
+                {introductionOptions.map((item, key) => (
+                  <MessageOption key={key} {...item} />
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-      </motion.div>
-    </>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
