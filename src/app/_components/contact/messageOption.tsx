93ca6a4a1c47fd2137baed4e65d42accbfb5d378
kalebhenrique <kalebhenriquebr@gmail.com>
2024-10-06 17:17:09 -0300

    Refactor Contact component and related files

---
+++ src/app/_components/contact/messageOption.tsx
@@ -0,0 +1,11 @@
+type MessageOptionProps = {
+  message: string;
+};
+
+export default function MessageOption(props: MessageOptionProps) {
+  return (
+    <div className="inline-block max-w-max rounded-full border-2 border-violeta-base p-4 text-violeta-base">
+      <p>{props.message}</p>
+    </div>
+  );
+}
---
+++ src/app/_components/contact/messageWrapper.tsx
@@ -0,0 +1,18 @@
+type MessageWrapperProps = {
+  message: string;
+  isUser: boolean;
+};
+
+export default function MessageWrapper(props: MessageWrapperProps) {
+  let bg = "bg-cinza-overlay-navbar";
+
+  if (props.isUser) {
+    bg = "bg-cinza";
+  }
+
+  return (
+    <div className={`inline-block max-w-max rounded-2xl ${bg} p-4`}>
+      <p>{props.message}</p>
+    </div>
+  );
+}
