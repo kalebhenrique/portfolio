364664e204cd29ab883fb899262fd4e17170e030
kalebhenrique <kalebhenriquebr@gmail.com>
2024-10-10 17:36:34 -0300

    Add progressive enhancement

--- src/components/introduction/introduction.tsx
+++ src/components/introduction/introduction.tsx
@@ -1,12 +1,8 @@
 "use client";
 import { motion } from "framer-motion";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "~/components/ui/popover";
-import ArrowDownAnimate from "./arrowDownAnimate";
-import CldImage from "../cldImage";
+import { useMediaQuery } from "react-responsive";
+import React from "react";
+import BackgroundContent from "./backgroundContent";
 
 const Snowflake = () => {
   return (
@@ -25,7 +21,16 @@ const Snowflake = () => {
 };
 
 export default function Introduction() {
-  const snowflakes = Array.from({ length: 100 });
+  const isDesktop = useMediaQuery({ minWidth: 768 });
+  const snowflakes = React.useMemo(() => Array.from({ length: 100 }), []);
+
+  if (!isDesktop) {
+    return (
+      <div className="relative flex h-[770px] w-full flex-col items-center justify-center overflow-hidden bg-gradient-to-r from-[#2C1431] via-[#161732] to-[#042433]">
+        <BackgroundContent />
+      </div>
+    );
+  }
 
   return (
     <motion.div
@@ -40,53 +45,14 @@ export default function Introduction() {
         repeat: Infinity,
         repeatType: "reverse",
       }}
-      className="n relative flex h-[770px] w-full flex-col items-center justify-center overflow-hidden bg-gradient-to-r from-[#2C1431] via-[#161732] to-[#042433]"
+      className="relative flex h-[770px] w-full flex-col items-center justify-center overflow-hidden bg-gradient-to-r from-[#2C1431] via-[#161732] to-[#042433]"
     >
       <div className="pointer-events-none absolute inset-0">
         {snowflakes.map((_, index) => (
           <Snowflake key={index} />
         ))}
       </div>
-
-      <motion.div
-        initial={{ opacity: 0, scale: 0.5 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{
-          duration: 0.8,
-          delay: 0.3,
-        }}
-        className="z-0 flex flex-col items-center"
-      >
-        <div className="flex flex-row items-end">
-          <Popover>
-            <PopoverTrigger>
-              <CldImage
-                width="360"
-                height="360"
-                src="celeste"
-                alt="Montanha Celeste"
-                className="z-0"
-              />
-            </PopoverTrigger>
-            <PopoverContent className="mr-2 w-32 bg-violeta-base bg-opacity-90 text-[11px] text-cinza-fundo backdrop-blur-sm">
-              <p>
-                Montanha do jogo Celeste, que aborda temas de superação e
-                perseverança — qualidades essenciais para programadores!
-              </p>
-            </PopoverContent>
-          </Popover>
-        </div>
-
-        <div className="py-3 text-center">
-          <span className="text-lg">
-            Sou apaixonado por criar experiências envolventes, acessíveis e
-            centradas no usuário.
-          </span>
-        </div>
-      </motion.div>
-      <div className="absolute bottom-5 left-0 right-0 flex justify-center">
-        <ArrowDownAnimate />
-      </div>
+      <BackgroundContent />
     </motion.div>
   );
 }
