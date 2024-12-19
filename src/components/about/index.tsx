8c5bbabaf066e09be83bf6dae39efed6008deaf9
kalebhenrique <kalebhenriquebr@gmail.com>
2024-12-19 14:52:12 -0300

    add responsivity to about me

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -9,22 +9,31 @@ export default function AboutMe() {
 
   return (
     <>
-      <section id="sobre" className="bg-roxo-fundo px-6 py-16">
-        <div className="flex flex-col items-center justify-center">
-          <FadeInWhenVisible>
-            <h1 className="text-center text-4xl text-violeta-titulo">
-              Eu sou um dev web e mobile front-end.
-            </h1>
-          </FadeInWhenVisible>
-          <FadeInWhenVisible delay={0.2}>
-            <CldImage
-              width="242"
-              height="242"
-              src="eu"
-              alt="Eu, Kaleb"
-              className="mx-auto my-10 flex rounded-3xl text-center"
-            />
-            <div className="space-y-3">
+      <section
+        id="sobre"
+        className="bg-roxo-fundo px-6 py-16 md:px-40 md:py-24 xl:px-60 xl:py-28"
+      >
+        <div className="flex flex-col md:flex-row md:space-x-28 xl:space-x-40">
+          <div className="space-y-6 md:space-y-20">
+            <FadeInWhenVisible>
+              <div className="flex flex-col items-center justify-center">
+                <h1 className="md:w-[80 0px] text-center text-4xl text-violeta-titulo md:text-start md:text-7xl">
+                  Eu sou um dev web e mobile front-end.
+                </h1>
+                <div className="md:hidden">
+                  <div className="w-full max-w-[242px] md:max-w-[600px]">
+                    <CldImage
+                      width="600"
+                      height="600"
+                      src="eu"
+                      alt="Eu, Kaleb"
+                      className="mx-auto my-10 w-full rounded-3xl object-cover"
+                    />
+                  </div>
+                </div>
+              </div>
+            </FadeInWhenVisible>
+            <FadeInWhenVisible delay={0.2}>
               <div className="space-y-3">
                 <p>
                   Estudante de graduação no sexto semestre de Engenharia de
@@ -36,19 +45,20 @@ export default function AboutMe() {
                   requisitos, com um forte interesse em gerência de projetos e
                   engenharia de software.
                 </p>
-
-                <div className="flex items-center justify-between py-5">
+                <div className="flex items-center justify-between md:justify-start md:space-x-5">
                   <p>Se te interessou, entre em contato:</p>
                   <Button
-                    className="rounded-full bg-[#7FC8E3] font-semibold text-roxo-fundo hover:bg-[#A4E3FA]"
+                    className="rounded-full bg-[#7FC8E3] font-semibold text-roxo-fundo hover:bg-[#A4E3FA] md:text-xl"
                     onClick={openChat}
                   >
                     Chat
                   </Button>
                 </div>
               </div>
+            </FadeInWhenVisible>
+            <FadeInWhenVisible delay={0.2}>
               <div>
-                <h2 className="text-lg text-violeta-titulo">
+                <h2 className="text-2xl text-violeta-titulo">
                   Motivação e Propósito
                 </h2>
                 <p>
@@ -57,8 +67,21 @@ export default function AboutMe() {
                   permita fazer a diferença.
                 </p>
               </div>
-            </div>
-          </FadeInWhenVisible>
+            </FadeInWhenVisible>
+          </div>
+          <div className="hidden md:inline">
+            <FadeInWhenVisible>
+              <div className="w-full">
+                <CldImage
+                  width="800"
+                  height="800"
+                  src="eu"
+                  alt="Eu, Kaleb"
+                  className="mx-auto my-10 w-full rounded-3xl object-cover"
+                />
+              </div>
+            </FadeInWhenVisible>
+          </div>
         </div>
       </section>
     </>
