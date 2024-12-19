caea4461ea4b6c523c351b62152da5e13afb3ec9
kalebhenrique <kalebhenriquebr@gmail.com>
2024-12-19 15:56:39 -0300

    otimize animation

--- src/components/introduction/arrowDownAnimate.tsx
+++ src/components/introduction/arrowDownAnimate.tsx
@@ -1,22 +1,9 @@
-"use client";
 import { IoChevronDown } from "react-icons/io5";
-import { motion } from "framer-motion";
 
 export default function ArrowDownAnimate() {
   return (
-    <motion.div
-      className="z-0 flex justify-center rounded-full bg-[#2C1431] bg-opacity-30 p-1"
-      animate={{
-        y: [0, 10, 0],
-      }}
-      transition={{
-        duration: 1.4,
-        repeat: Infinity,
-        repeatType: "mirror",
-        ease: "easeInOut",
-      }}
-    >
+    <div className="z-0 flex justify-center rounded-full bg-[#2C1431] bg-opacity-30 p-1 motion-safe:animate-bounce">
       <IoChevronDown className="text-3xl md:text-4xl" />
-    </motion.div>
+    </div>
   );
 }
